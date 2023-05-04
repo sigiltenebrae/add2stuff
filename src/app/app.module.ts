f87b41d21a4553b425301e25c8ba7ae02e01e1bc
sigiltenebrae <ck6202@rit.edu>
2023-05-04 14:05:07 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CatViewComponent } from './cat-view/cat-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatViewComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/cat-view/cat-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-view',
+  templateUrl: './cat-view.component.html',
+  styleUrls: ['./cat-view.component.css']
+})
+export class CatViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
