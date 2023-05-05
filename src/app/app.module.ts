b62aaf0558ac9cf82f54cec780850669db1fd024
sigiltenebrae <ck6202@rit.edu>
2023-05-05 10:32:41 -0400

    angular update and material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CatViewComponent } from './cat-view/cat-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { CatViewComponent } from './cat-view/cat-view.component';
     CatViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
